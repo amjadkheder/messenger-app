29c931f8b4bb91089445cb7b7aded0b5aa22bf17
amjadkheder <amjad.kheder@gmail.com>
2022-12-25 13:34:57 +0300

    Initial Commit

---
+++ massenger app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  massenger app
+//
+//  Created by Amjad on 01/06/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ massenger appUITests/massenger_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  massenger_appUITestsLaunchTests.swift
+//  massenger appUITests
+//
+//  Created by Amjad on 01/06/1444 AH.
+//
+
+import XCTest
+
+class massenger_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
